3fae9f4a7063ffc3425108ed4b044f9de71006b3
Anas Mouqine  (kaelar) <anasmouquine2@gmail.com>
2025-04-30 22:50:37 +0100

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -715,6 +715,16 @@ class DOMUtils {
         this.handlePasswordReset();
       });
       
+       // Check auth state on page load
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      console.log('User is signed in on page load');
+      this.updateAuthUI();
+    } else {
+      console.log('No user is signed in on page load');
+      this.updateAuthUI();
+    }
+  });
       // Password change form
       const passwordForm = document.querySelector('.profile__password-form');
       passwordForm?.addEventListener('submit', e => {
